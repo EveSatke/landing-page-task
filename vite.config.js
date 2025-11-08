4984a963453c86031f3f313a650a1d95c950e6f5
Evelina Satkauske <evelina.satkauske@gmail.com>
2025-11-08 20:07:02 +0200

    Add gh-pages deployment script and update package dependencies; adjust Vite config for base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
+  base: '/landing-page-task/',
   server: { port: 5173, open: true },
-  build: { outDir: 'dist', emptyOutDir: true }
+  build: { outDir: 'dist', emptyOutDir: true },
 });
